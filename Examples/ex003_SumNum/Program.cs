67485f53cb1ea39eb1bc5b2eb5900b387976fa67
4lorence <nikolay.evdo@gmail.com>
2022-11-28 13:48:47 +0500

    add to ex003 random

--- Examples/ex003_SumNum/Program.cs
+++ Examples/ex003_SumNum/Program.cs
@@ -1,3 +1,5 @@
-﻿int NumberA = 3;
-int NumberB = 4;
+﻿int NumberA = new Random().Next(1,10);
+Console.WriteLine(NumberA);
+int NumberB = new Random().Next(1,10);
+Console.WriteLine(NumberB);
 Console.WriteLine(NumberA+NumberB);
