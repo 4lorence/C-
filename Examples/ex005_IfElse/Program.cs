0fd5c701676700e3ab76ed5458ee2ff953cba61c
4lorence <nikolay.evdo@gmail.com>
2022-11-28 14:14:07 +0500

    add ex005_IfElse and add ToLower

---
+++ Examples/ex005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("enter user name");
+string username = Console.ReadLine();
+
+if(username.ToLower() == " maria")
+{
+    Console.WriteLine("Wow, its Maria");
+}
+else
+{
+    Console.Write("hello,");
+    Console.WriteLine(username);
+}
