04e0e4734c5b361d857ebfb0e914d98653f2d78e
4lorence <nikolay.evdo@gmail.com>
2022-12-01 10:49:42 +0500

    add ex009_IntroArray

---
+++ Examples/ex010_MethodArray/Program.cs
@@ -0,0 +1,23 @@
+﻿int[]array ={1,2,3,4,5,6,7,8,9,10};
+
+int n =array.Length;
+int find = 11;
+
+int i = 0;
+
+while(i < n)
+{
+    if(array[i] == find)
+    {
+        Console.WriteLine(i);
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Oops");
+        break;
+    }
+    //index = index +1;
+    i++;
+}
+
