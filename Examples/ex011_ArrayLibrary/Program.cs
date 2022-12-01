8aa7432b07ce40ab45070e71aa80dc99768ef678
4lorence <nikolay.evdo@gmail.com>
2022-12-01 13:17:07 +0500

    changes ex011_IntroArray

--- Examples/ex011_ArrayLibrary/Program.cs
+++ Examples/ex011_ArrayLibrary/Program.cs
@@ -20,7 +20,28 @@ void PrintArray(int[]col)
     }
 }
 
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
 int[] array = new int[10];
 
 FillArray(array);
 PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
