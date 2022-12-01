a98bc857ea1e977390c6da02913b20ff82f051c9
4lorence <nikolay.evdo@gmail.com>
2022-12-01 12:15:53 +0500

    add ex011_ArrayLibrary

---
+++ Examples/ex011_ArrayLibrary/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+void PrintArray(int[]col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
